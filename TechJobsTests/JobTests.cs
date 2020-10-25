ebb3f19fbafe2c96a7a88e05c2ca48050c8e44b2
Michael Hacker <hack1973@swbell.net>
2020-10-25 02:16:52 -0500

    completed empty/full constructors and equals() tests

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -17,8 +17,29 @@ namespace TechJobsTests
         {
             Job job4 = new Job();
             Job job5 = new Job();
-            Assert.AreNotEqual(job4.Id,job5.Id);
+            Assert.AreNotEqual(job4.Id, job5.Id);
             Assert.IsTrue(job5.Id == job4.Id + 1);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            //Declare and initialize a new Job object with the following data: "Product tester" for Name, "ACME" for EmployerName,
+            //"Desert" for JobLocation, "Quality control" for JobType, and "Persistence" for JobCoreCompetency
+            Job job6 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual("Product tester", job6.Name);
+            Assert.AreEqual("ACME", job6.EmployerName.ToString());
+            Assert.AreEqual("Desert", job6.EmployerLocation.ToString());
+            Assert.AreEqual("Quality control", job6.JobType.ToString());
+            Assert.AreEqual("Persistence", job6.JobCoreCompetency.ToString());
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Prodct tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.IsFalse(Equals(job2, job1));
         }
     }
 }
